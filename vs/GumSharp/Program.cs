d9b2f92fcbaba5cd42bfde25618301e14ef8e4ac
Fabien Bavent <fabien.bavent@gmail.com>
2018-07-02 19:32:27 +0200

    Improve C# Library

--- vs/GumSharp/Program.cs
+++ vs/GumSharp/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace GumSharp
 {
@@ -59,9 +60,62 @@ namespace GumSharp
             return cellPtr != IntPtr.Zero ? new Cell(cellPtr) : null;
         }
 
-
+        public Cell GetById(string id)
+        {
+            IntPtr ptr = GetById_(Pointer, id);
+            return GetSingle(ptr);
+        }
+   
+        public void DestroyChildren()
+        {
+            DestroyChildren_(Pointer);
+            // TODO - what about cache !?
+            cellCache.Clear();
+        }
+        
+        public void Append(Cell cell)
+        {
+            Append_(Pointer, cell.Pointer);
+        }
+        
+        public Cell Clone()
+        {
+            IntPtr ptr = Clone_(Pointer);
+            return GetSingle(ptr);
+        }
+        
+        public Cell Remove()
+        {
+            Remove_();
+            return this;
+        }
+        
+        static Dictionary<IntPtr, Cell> cellCache = new Dictionary<IntPtr, Cell>();
+        
+        private static Cell GetSingle(IntPtr ptr)
+        {
+            lock(cellCache) {
+                Cell cell;
+                if (!cellCache(ptr, out cell)) {
+                    cell = new Cell(ptr);
+                    cellCache.Add(ptr, cell);
+                }
+                return cell;
+            }
+        }
+   
         [DllImport("gum", CallingConvention = CallingConvention.Cdecl, EntryPoint = "gum_cell_loadxml")]
         private extern static IntPtr LoadXml_(string filename, IntPtr skins);
+        [DllImport("gum", CallingConvention = CallingConvention.Cdecl, EntryPoint = "gum_get_by_id")]
+        private extern static IntPtr GetById_(IntPtr cell, string id);
+        [DllImport("gum", CallingConvention = CallingConvention.Cdecl, EntryPoint = "gum_cell_destroy_child")]
+        private extern static void DestroyChildren_(IntPtr cell);
+        [DllImport("gum", CallingConvention = CallingConvention.Cdecl, EntryPoint = "gum_cell_pushback")]
+        private extern static void Append_(IntPtr cell, IntPtr child);
+        [DllImport("gum", CallingConvention = CallingConvention.Cdecl, EntryPoint = "gum_cell_copy")]
+        private extern static IntPtr Clone_(IntPtr cell);
+        [DllImport("gum", CallingConvention = CallingConvention.Cdecl, EntryPoint = "gum_cell_detach")]
+        private extern static void Remove_(IntPtr cell);
     }
 
     class SkinsPool
@@ -81,6 +135,34 @@ namespace GumSharp
         private extern static IntPtr LoadCss_(IntPtr skins, string filename);
     }
 
+    enum EventType
+    {
+        Unknow,
+        Destroy,
+        Expose,
+        KeyPress,
+        KeyUp,
+        Motion,
+        ButtonPress,
+        ButtonOut,
+        ButtonOver,
+        ButtonDown,
+        ButtonUp,
+        ButtonFocus,
+        ButtonLooseFocus,
+        Click,
+        DoubleClick,
+        TripleClick,
+        RightClick,
+        
+        Previous,
+        Next,
+        WheelUp,
+        WheelDown,
+        WheelClick,
+        Resize
+    }
+    
     class Manager : IDisposable
     {
         internal IntPtr Pointer { get; private set; }
@@ -107,7 +189,10 @@ namespace GumSharp
         {
             EventLoop_(Pointer);
         }
-
+        
+        public void Bind(string id, EventType type, MethodInfo method)
+        {
+        }
 
         private delegate void InternEventHandler();
         [DllImport("gum", CallingConvention = CallingConvention.Cdecl, EntryPoint = "gum_event_manager")]
@@ -120,16 +205,114 @@ namespace GumSharp
         private extern static IntPtr BindEvent_(IntPtr manager, IntPtr cell, int type, InternEventHandler handler);    
     }
 
-    class Program
+    class GumApp: IDisposable
     {
+        public SkinsPool Skins { get; private set; }
+        public Cell Root { get; private set; }
+        public Window Window { get; private set; }
+        public Manager Manager { get; private set; }
+        
+        protected GumApp()
+        {
+            string name = "browser/app2";
+            Skins = new SkinsPool();
+            Skins.LoadCss("./resx/" + name + ".css");
+            Root = Cell.Load("./resx/" + name + ".xml");
+            int width = 680; // TODO - Read from root
+            int height = 425;
+            Window = Window.Open(width, height);
+            Manager = Manager.Create(Root, Window);
+            
+            foreach (var method in GetType().GetMethods()) {
+                BindEventAttribute[] attributes = (BindEventAttribute[])method.GetCustomAttributes(typeof(BindEventAttribute), false);
+                foreach (var attribute in attributes) {
+                    Manager.Bind(attribute.Id, attrinute.Type, method);
+                }
+            }
+        }
+        
+        public void Dispose()
+        {
+            if (Manager != null)
+                Manager.Dispose();
+            if (Window != null)
+                Window.Dispose();
+        }
+    }
+    
+    class BindEventAttribute: Attribute
+    {
+        public BindEventAttribute(EventType type): this(null, type)
+        {
+        }
+        
+        public BindEventAttribute(string id, EventType type)
+        {
+            Id = id;
+            Type = type;
+        }
+        
+        public string Id { get; private set; }
+        public EventType Type { get; private set; }
+    }
+
+    class Browser: GumApp
+    {
+        FileInfo directory;
+        Cell view;
+        Cell icon;
+        Cell ctxFile;
+        Cell ctxView;
+        
+        public Browser(): this(Environment.CurrentDirectory)
+        {
+        }
+        
+        public Browser(string dir)
+        {
+            directory = new FileInfo(dir);
+            view = Root.GetById("view");
+            icon = Root.GetById("icon");
+            ctxFile = Root.GetById("ctx-menu-file").Remove();
+            ctxView = Root.GetById("ctx-menu-view").Remove();
+            OnRefresh(null);
+        }
+        
         static void Main(string[] args)
         {
-            SkinsPool skins = new SkinsPool();
-            skins.LoadCss("./resx/browser/app2.css");
-            Cell root = Cell.Load("./resx/browser/app2.xml", skins);
-            using (Window win = Window.Open(680, 425))
-            using (Manager ewm = Manager.Create(root, win)) {
-                ewm.Run();
+            using (GumApp app = Browser())
+                app.Manager.Run();
+        }
+        
+        [BindEvent(EventType.Previous)]
+        [BindEvent("btn-prev", EventType.Click)]
+        public void OnPrevious(Cell target)
+        {
+        }
+        
+        [BindEvent(EventType.Next)]
+        [BindEvent("btn-next", EventType.Click)]
+        public void OnNext(Cell target)
+        {
+        }
+        
+        [BindEvent("btn-top", EventType.Click)]
+        public void OnParent(Cell target)
+        {
+            directory = new FileInfo(directory.DirectoryName);
+            OnRefresh(target);
+        }
+        
+        [BindEvent("btn-refr", EventType.Click)]
+        public void OnRefresh(Cell target)
+        {
+            while(directory != null && (!directory.Exists || !directory.Attributes.HasFlag(FileAttributes.Directory)))
+                directory = new FileInfo(directory.DirectoryName);
+            if (directory == null)
+                directory = new FileInfo("C:/");
+            foreach (string file in Directory.EnumerateFileEntries(directory.Fullname)) {
+                // icon.SetTextAndIcon();
+                // view.Append(icon.Clone());
             }
         }
     }
